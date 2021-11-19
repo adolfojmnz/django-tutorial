4c42fbe5ca6b064cdaa2a498181f4f1ac6fd950c
Eadwulf <eadwulfts@protonmail.com>
2021-11-19 10:32:13 -0400

    add year_archive, month_archive and detail_archive views

--- tutorial01/views.py
+++ tutorial01/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Article
+
+
+def year_archive(request, year):
+    template_name = 'templates/archive_list.html'
+    context = {
+        'year': year,
+        'archive_list': Article.objects.filter(
+			pub_date__year=year
+		),
+    }
+    return render(request, template_name, context)
+
+
+def month_archive(request, year, month):
+	template_name = 'templates/archive_list.html'
+	context = {
+		'year': year,
+		'month': month,
+		'archive_list': Article.objects.filter(
+			pub_date__year=year,
+			pub_date__month=month
+		),
+	}
+	return render(request, template_name, context)
+
+
+def detail_archive(request, year, month, pk):
+	template_name = 'templates/archive_detail.html'
+	context = {
+		'year': year,
+		'month': month,
+		'pk': pk,
+		'archive_detail': Article.objects.get(pk=pk),
+	}
+	return render(request, template_name, context)
