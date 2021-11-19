91fdc5dcc8178643dbf53c2e190a1e7f59227fb7
Eadwulf <eadwulfts@protonmail.com>
2021-11-19 12:34:03 -0400

    name attribute changed to match templates' name.

--- tutorial01/urls.py
+++ tutorial01/urls.py
@@ -4,7 +4,7 @@ from .views import year_archive, month_archive, detail_archive
 
 
 urlpatterns = [
-        path('archive/<int:year>/', year_archive, name='year_archive'),
-		path('archive/<int:year>/<int:month>/', month_archive, name='month_archive'),
-		path('archive/<int:year>/<int:month>/<int:pk>/', detail_archive, name='detail_archive'),
+        path('archive/<int:year>/', year_archive, name='archive_list'),
+		path('archive/<int:year>/<int:month>/', month_archive, name='archive_list'),
+		path('archive/<int:year>/<int:month>/<int:pk>/', detail_archive, name='archive_detail'),
 ]
