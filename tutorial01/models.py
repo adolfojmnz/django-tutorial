f54c76c251040d5de58c20a7dd9ecc492bca2aaf
Eadwulf <eadwulfts@protonmail.com>
2021-11-19 10:29:30 -0400

    add tutorial01/models.py for Article and Reporter

--- tutorial01/models.py
+++ tutorial01/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Reporter(models.Model):
+    full_name = models.CharField(max_length=70)
+
+    class Meta:
+        ordering = ['full_name']
+
+    def __str__(self):
+        return self.full_name
+
+
+class Article(models.Model):
+    pub_date = models.DateField()
+    headline = models.CharField(max_length=200)
+    content  = models.TextField()
+    author   = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.headline
