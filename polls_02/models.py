3df9f46c74ac8180e62c171c1b8d0e84032aa5d0
Eadwulf <eadwulfts@protonmail.com>
2021-11-20 16:49:22 -0400

    delete blank lines

--- polls_02/models.py
+++ polls_02/models.py
@@ -3,7 +3,6 @@ from datetime import date
 
 
 class Question(models.Model):
-
 	question_text = models.CharField(max_length=200)
 	pub_date 	  = models.DateField(default=date.today)
 
@@ -12,7 +11,6 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-
 	question 	= models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes	    = models.IntegerField(default=0)
