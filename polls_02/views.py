7a4a9eac9388f1f490afd5aa0de1988fa903d023
Eadwulf <eadwulfts@protonmail.com>
2021-11-20 10:26:11 -0400

    add polls views

--- polls_02/views.py
+++ polls_02/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Question
+
+
+def poll_list(request):
+	template_name = 'polls/index.html'
+	context = {
+		'poll_list': Question.objects.all()
+	}
+	return render(request, template_name, context)
+
+
+def poll_details(request, question_id):
+	template_name = 'polls/poll_detail.html'
+	context = {
+		'question_id': question_id
+	}
+	return render(request, template_name, context)
+
+
+def poll_results(request, question_id):
+	template_name = 'polls/poll_results.html'
+	context = {
+		'question_id': question_id
+	}
+	return render(request, template_name, context)
+
+
+def poll_vote(request, question_id):
+	template_name = 'polls/poll_vote.html'
+	context = {
+		'question_id': question_id
+	}
+	return render(request, template_name, context)
