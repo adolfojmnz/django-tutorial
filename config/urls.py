a9569d9ec47af1f317d9ecd34dd5cf0bf92b9f67
Eadwulf <eadwulfts@protonmail.com>
2021-11-20 10:27:56 -0400

    add polls' urls

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('', include('tutorial01.urls'))
+	path('', include('tutorial01.urls')),
+	path('', include('polls_02.urls')),
 ]
