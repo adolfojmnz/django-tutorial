cfd89aaa82a8ad4da2ea8b6b222b5fce1fd0e9d2
Eadwulf <eadwulfts@protonmail.com>
2021-11-21 06:46:55 -0400

    add app_name and rename templates

--- polls_02/urls.py
+++ polls_02/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from .views import poll_list, poll_details, poll_vote, poll_results
 
 
+app_name = 'polls'
 urlpatterns = [
 	path('polls/', poll_list, name='poll_list'),
-	path('polls/<int:question_id>/vote/', poll_vote, name='poll_vote'),
-	path('polls/<int:question_id>/details/', poll_details, name='poll_details'),
-	path('polls/<int:question_id>/results/', poll_results, name='poll_results'),
+	path('polls/<int:question_id>/vote/', poll_vote, name='vote'),
+	path('polls/<int:question_id>/details/', poll_details, name='details'),
+	path('polls/<int:question_id>/results/', poll_results, name='results'),
 ]
