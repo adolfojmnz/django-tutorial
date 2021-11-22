12f74c8f896e2f89dde47ea3622bd80ea03d1313
Eadwulf <eadwulfts@protonmail.com>
2021-11-22 16:35:14 -0400

    Add logic to poll_vote and poll_results. Delete unnecesary variable 'choices' from poll_details. poll_list rename to poll_index

--- polls_02/views.py
+++ polls_02/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Question, Choice
 
 
-def poll_list(request):
+def poll_index(request):
 	template_name = 'polls/index.html'
 	context = {
 		'question_list': Question.objects.order_by('-pub_date')[:5]
@@ -14,27 +16,39 @@ def poll_list(request):
 def poll_details(request, question_id):
 	template_name = 'polls/poll_detail.html'
 	question = get_object_or_404(Question, pk=question_id)
-	choices = get_list_or_404(question.choice_set.all())
 
 	context = {
 		'question': question,
-		'choices': choices,
-	}
-	return render(request, template_name, context)
-
-
-def poll_results(request, question_id):
-	template_name = 'polls/poll_results.html'
-	context = {
-		'question': get_object_or_404(
-			Question, pk=question_id
-		)
 	}
 	return render(request, template_name, context)
 
 
 def poll_vote(request, question_id):
 	template_name = 'polls/poll_vote.html'
+	question = get_object_or_404(Question, pk=question_id)
+
+	try:
+		selected_choice = question.choice_set.get(
+			pk=request.POST['choice']
+		)
+		selected_choice.votes += 1
+		selected_choice.save()
+	except (KeyError, Choice.DoesNotExist):
+		context = {
+			'question': question,
+			'error_message': 'Select an option!'
+		}
+		return render(request, template_name, content)
+
+	return HttpResponseRedirect(
+		reverse(
+			'polls:results', args=(question.id,)
+		)
+	)
+
+
+def poll_results(request, question_id):
+	template_name = 'polls/poll_results.html'
 	context = {
 		'question': get_object_or_404(
 			Question, pk=question_id
