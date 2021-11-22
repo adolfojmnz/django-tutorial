706da7780209561a5d1266a7f94b008bf3d82349
Eadwulf <eadwulfts@protonmail.com>
2021-11-22 16:31:27 -0400

    rename function view and path name to poll_index and index respectively

--- polls_02/urls.py
+++ polls_02/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import poll_list, poll_details, poll_vote, poll_results
+from .views import poll_index, poll_details, poll_vote, poll_results
 
 
 app_name = 'polls'
 urlpatterns = [
-	path('polls/', poll_list, name='poll_list'),
+	path('polls/', poll_index, name='index'),
 	path('polls/<int:question_id>/vote/', poll_vote, name='vote'),
 	path('polls/<int:question_id>/details/', poll_details, name='details'),
 	path('polls/<int:question_id>/results/', poll_results, name='results'),
